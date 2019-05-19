aa58260d2a522c8d66184cb1639008634091e368
xing.xiao <xiao_1069@163.com>
2019-05-19 23:19:35 +0800

    commit

--- user/views.py
+++ user/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from .models import *
 from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest, JsonResponse
+from django.conf import settings
+import bcrypt
+import jwt
+import datetime
 import simplejson
 import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(thread)d %(message)s")
 # Create your views here.
+
+
+def get_token(user_id):
+    '''生成token'''
+    return jwt.encode({     # 加入时间戳，判断是否重发token或重新登录
+        'user_id': user_id,
+        'timestamp': int(datetime.datetime.now().timestamp())   # 取整
+    }, settings.SECRET_KEY, 'HS256').decode()       # 字符串
 
 
 def reg(request: HttpRequest):
@@ -12,14 +24,16 @@ def reg(request: HttpRequest):
     print(payload)
     logging.info(payload)
     try:
+        # 有任何错误都返回400，如果保存数据错误，则向外抛出异常
         email = payload['email']
         query = User.objects.filter(email=email)
         print(query)
         print(query.query)
         if query:
-            return HttpResponseBadRequest()
+            return HttpResponseBadRequest()     # 返回实例，不是异常类
+
         name = payload['name']
-        password = payload['password']
+        password = bcrypt.hashpw(payload['password'].encode(), bcrypt.gensalt())
         print(email, name, password)
 
         user = User()
@@ -28,7 +42,7 @@ def reg(request: HttpRequest):
         user.email = email
         try:
             user.save()
-            return JsonResponse({'user': user.id})
+            return JsonResponse({'token': get_token(user.id)})
         except:
             raise
     except Exception as e:
